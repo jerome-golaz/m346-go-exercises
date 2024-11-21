fcb28c3d1bb314ce50cfa8cef3739ea15cbec5a6
Jérôme Golaz <golaz.jerome@gmail.com>
2024-11-21 16:28:40 +0100

    init

--- go-1-ex-1/main.go
+++ go-1-ex-1/main.go
@@ -4,6 +4,15 @@ import "fmt"
 
 func main() {
 	// TODO: Declare and initialize the variables being used in the output!
+	firstName := "Jérôme"
+	lastName := "Golaz"
+	dayOfBirth := 29
+	monthOfBirth := 04
+	yearOfBirth := 2008
+	numberOfSiblings := 2
+	heightInMeters := 1.82
+	zodiacSign := ""
+
 	fmt.Printf("Vor- und Nachname: %s %s\n", firstName, lastName)
 	fmt.Printf("Geburtsdatum: %d.%d.%d\n", dayOfBirth, monthOfBirth, yearOfBirth)
 	fmt.Printf("Anzahl Geschwister: %d\n", numberOfSiblings)
