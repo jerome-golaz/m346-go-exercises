0a893d9754649a62907c8713b06e9f2878fb6f4a
Jérôme Golaz <golaz.jerome@gmail.com>
2024-11-28 16:16:30 +0100

    go-1-ex-1 to go-2-ex-1

--- go-2-ex-1/main.go
+++ go-2-ex-1/main.go
@@ -4,12 +4,21 @@ import "fmt"
 
 type FullName struct {
 	// TODO: add fields
+	FirstName string
+	LastName  string
 }
 
 // TODO: declare a structure for birth date
+type BirthDate struct {
+	Year  int
+	Month int
+	Day   int
+}
 
 type Profile struct {
 	// TODO: embed full name and birth date information
+	FullName         FullName
+	BirthDate        BirthDate
 	NumberOfSiblings byte
 	ZodiacSign       rune
 }
